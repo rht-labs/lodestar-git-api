89eb68e824519717ad9f6daa0e7d6750a8d62c3d
Frederick Permantier <frederick.permantier@wavesoup.be>
2020-01-17 11:45:46 +0000

    created real integration tests

--- src/main/java/com/redhat/labs/omp/config/GitLabConfig.java
+++ src/main/java/com/redhat/labs/omp/config/GitLabConfig.java
@@ -1,13 +1,26 @@
 package com.redhat.labs.omp.config;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class GitLabConfig {
+    public static final String GITLAB_PERSONAL_ACCESS_TOKEN = "GITLAB_PERSONAL_ACCESS_TOKEN";
+    public static Logger logger = LoggerFactory.getLogger(GitLabConfig.class);
+
     // defined as a static method because we need access to the token from @ClientHeaderParam
     public static String getPersonalAccessToken() {
-        return System.getenv().get("GITLAB_PERSONAL_ACCESS_TOKEN");
+        String gitLabPersonalToken = System.getProperty(GITLAB_PERSONAL_ACCESS_TOKEN);
+        if (gitLabPersonalToken == null) {
+            gitLabPersonalToken = System.getenv().get(GITLAB_PERSONAL_ACCESS_TOKEN);
+        }
+
+        if (gitLabPersonalToken == null) {
+            logger.warn(GITLAB_PERSONAL_ACCESS_TOKEN + " environment variable/system property not set!");
+        }
+
+        return gitLabPersonalToken;
     }
 }
