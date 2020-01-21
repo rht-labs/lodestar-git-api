65490da39cbfdaa19086e42797a3f08398aef341
Faisal Masood <fmasood@redhat.com>
2020-01-21 14:22:24 +0000

    GitSyncManage now starts a hotrod sever

--- src/test/java/com/redhat/labs/cache/GitSyncManagerTest.java
+++ src/test/java/com/redhat/labs/cache/GitSyncManagerTest.java
@@ -6,6 +6,16 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+import org.infinispan.server.hotrod.HotRodServer;
+import org.infinispan.server.hotrod.configuration.HotRodServerConfigurationBuilder;
+import org.infinispan.server.hotrod.test.HotRodTestingUtil;
+import org.infinispan.test.fwk.TestCacheManagerFactory;
+import org.infinispan.test.fwk.TestResourceTracker;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -26,6 +36,40 @@ class GitSyncManagerTest {
     @Inject
     GitSyncManager gitSyncManager;
 
+
+    @BeforeAll
+    public static void init() {
+//        Map<String, String> a = server.start();
+//
+//        RemoteCacheManager rcm = server.hotRodClient();
+////        rcm.start();
+//
+//
+////        System.out.println("******************\n\n\n" + rcm.getCacheNames() + "\n\n\n\n************");
+//        rcm.administration().createCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME, "org.infinispan.DIST_SYNC");
+////        rcm.administration().createCache("myCache", (BasicConfiguration) null);
+
+        TestResourceTracker.setThreadTestName("InfinispanServer");
+//        GlobalConfigurationBuilder builder = new GlobalConfigurationBuilder();
+//
+//
+//        builder.marshaller(JavaSerializationMarshaller.class)
+//                .addJavaSerialWhiteList("org.infinispan.example.*", "org.infinispan.concrete.SomeClass");
+
+
+        EmbeddedCacheManager ecm = TestCacheManagerFactory.createCacheManager(
+                new GlobalConfigurationBuilder().nonClusteredDefault().defaultCacheName("default"),
+                new ConfigurationBuilder());
+        ecm.createCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME, new ConfigurationBuilder().indexing().build());
+        ecm.createCache("myCache", new ConfigurationBuilder().indexing().build());
+        // Client connects to a non default port
+        HotRodServerConfigurationBuilder hcb = new HotRodServerConfigurationBuilder();
+
+        HotRodServer hs =  HotRodTestingUtil.startHotRodServer(ecm, 11222);
+//        hs.setMarshaller(new org.infinispan.commons.marshall.JavaSerializationMarshaller());
+
+
+    }
 
     @Test
     public void testEventReceivedForCreateProject(VertxTestContext vertxTestContext) throws Throwable {
