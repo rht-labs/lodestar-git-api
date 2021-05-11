223f712ac147bd93f2035a0f100ad9d1d369ad61
Derek Wasinger <dwasinge@redhat.com>
2021-05-11 09:54:28 -0700

    Remove Runtime Config API (#132)
    * remove config file api
    
    * remove runtime configmap from deployment

--- src/test/java/com/redhat/labs/lodestar/service/ConfigServiceTest.java
+++ src/test/java/com/redhat/labs/lodestar/service/ConfigServiceTest.java
@@ -2,7 +2,6 @@ package com.redhat.labs.lodestar.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
@@ -13,80 +12,14 @@ import javax.json.bind.config.PropertyNamingStrategy;
 import org.junit.jupiter.api.Test;
 
 import com.redhat.labs.lodestar.config.JsonMarshaller;
-import com.redhat.labs.lodestar.exception.FileNotFoundException;
 import com.redhat.labs.lodestar.mocks.MockGitLabService;
-import com.redhat.labs.lodestar.models.gitlab.File;
 import com.redhat.labs.lodestar.models.gitlab.HookConfig;
 import com.redhat.labs.lodestar.rest.client.GitLabService;
-import com.redhat.labs.lodestar.utils.ResourceLoader;
 
 class ConfigServiceTest {
-
-    @Test void testGetConfigFilePreLoaded() {
-        ConfigService service = new ConfigService();
-        service.configFile = "src/test/resources/lodestar-runtime-config.yaml";
-        service.webHooksFile = "src/test/resources/webhooks.yaml";
-        service.marshaller = new JsonMarshaller();
-
-        ProjectService projectService = new ProjectService();
-        projectService.gitLabService = new MockGitLabService();
-        service.projectService = projectService;        
-        service.engagementRepositoryId = 0;
-
-        service.reloadConfig = true;
-        service.reloadConfigMapData();
-        File config = service.getConfigFile();
-
-        assertNotNull(config);
-        assertEquals("src/test/resources/lodestar-runtime-config.yaml", config.getFilePath());
-        assertEquals(ResourceLoader.load("lodestar-runtime-config.yaml"), config.getContent());
-
-    }
-
-    @Test void testGetConfigFileFromGitLab() {
-        ConfigService service = new ConfigService();
-        service.configFile = "runtime/lodestar-runtime-config.yaml";
-        service.webHooksFile = "src/test/resources/webhooks.yaml";
-        service.marshaller = new JsonMarshaller();
-
-        GitLabService gitLabService = new MockGitLabService();
-
-        FileService fileService = new FileService();
-        fileService.gitLabService = gitLabService;
-        service.fileService = fileService;
-
-        ProjectService projectService = new ProjectService();
-        projectService.gitLabService = gitLabService;
-        service.projectService = projectService;        
-        service.engagementRepositoryId = 0;
-
-        service.reloadConfigMapData();
-        File config = service.getConfigFile();
-
-        assertNotNull(config);
-        assertEquals("runtime/lodestar-runtime-config.yaml", config.getFilePath());
-        assertEquals(ResourceLoader.load("lodestar-runtime-config.yaml"), config.getContent());
-
-    }
-
-    @Test void testGetConfigFileNotFound() {
-        ConfigService service = new ConfigService();
-        service.configFile = "schema/notfound.yaml";
-        
-        FileService fileService = new FileService();
-        fileService.gitLabService = new MockGitLabService();
-        service.fileService = fileService;
-        
-        Exception exception = assertThrows(FileNotFoundException.class, () -> {
-            service.getConfigFile();
-        });
-        
-        assertEquals("the configured file was not found in the gitlab repository.", exception.getMessage());
-    }
     
     @Test void testGetHookConfigPreLoaded() {
         ConfigService service = new ConfigService();
-        service.configFile = "src/test/resources/lodestar-runtime-config.yaml";
         service.webHooksFile = "src/test/resources/webhooks.yaml";
         service.marshaller = new JsonMarshaller();
 
@@ -107,7 +40,6 @@ class ConfigServiceTest {
     
     @Test void testGetHookConfigLabNotEmpty() {
         ConfigService service = new ConfigService();
-        service.configFile = "src/test/resources/lodestar-runtime-config.yaml";
         service.webHooksFile = "/runtime/webhooks.yaml";
         service.marshaller = new JsonMarshaller();
         
@@ -125,7 +57,6 @@ class ConfigServiceTest {
     
     @Test void testGetHookConfigLabsEmpty() {
         ConfigService service = new ConfigService();
-        service.configFile = "src/test/resources/lodestar-runtime-config.yaml";
         service.webHooksFile = "schema/notfound.yaml";
         service.marshaller = new JsonMarshaller();
         
@@ -149,7 +80,6 @@ class ConfigServiceTest {
         jm.setJsonb(JsonbBuilder.create(config));
 
         ConfigService service = new ConfigService();
-        service.configFile = "runtime/lodestar-runtime-config.yaml";
         service.webHooksFile = "src/test/resources/webhooks.yaml";
         service.marshaller = jm;
 
