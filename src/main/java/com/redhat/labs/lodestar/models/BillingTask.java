89e9f560437d2f4a03e49177b98e364434923dd2
Kirill Malgichev <kmalgich@redhat.com>
2021-06-10 17:43:30 -0400

    Billing Codes added

---
+++ src/main/java/com/redhat/labs/lodestar/models/BillingTask.java
@@ -0,0 +1,15 @@
+package com.redhat.labs.lodestar.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingTask {
+
+    private String code;
+}
